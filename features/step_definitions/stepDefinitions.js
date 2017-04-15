8f919a5abd583106bd6d709d472e12a682664072
mallorym <mallory@moduscreate.com>
2017-04-15 11:22:22 -0500

    Update step def
    added assertion to first step.

--- features/step_definitions/stepDefinitions.js
+++ features/step_definitions/stepDefinitions.js
@@ -1,5 +1,4 @@
 // features/step_definitions/stepDefinitions.js
-
 var angularPage = require('../pages/homePage.js');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
@@ -9,26 +8,29 @@ var expect = chai.expect;
 
 
 module.exports = function() {
-  this.Given(/^I go to "([^"]*)"$/, function(site) {
+  this.Given(/^I go to "([^"]*)"$/, function(site, next) {
     //browser.get(site);
     angularPage.go(site);
+    expect(browser.getTitle()).to.eventually.equal('AngularJS — Superheroic JavaScript MVW Framework').and.notify(next);
   });
 
-  this.When(/^I add "([^"]*)" in the task field$/, function(task) {
+  this.When(/^I add "([^"]*)" in the task field$/, function(task, next) {
     //element(by.model('todoList.todoText')).sendKeys(task);
     angularPage.addTask(task);
+    next();
   });
 
-  this.When(/^I click the add button$/, function() {
+  this.When(/^I click the add button$/, function(next) {
     //var el = element(by.css('[value="add"]'));
     //el.click();
     angularPage.submitTask();
+    next();
   });
 
-  this.Then(/^I should see my new task in the list$/, function(callback) {
+  this.Then(/^I should see my new task in the list$/, function(next) {
     var todoList = angularPage.angularHomepage.todoList;
     expect(todoList.count()).to.eventually.equal(3);
     expect(todoList.get(2).getText()).to.eventually.equal('Be Awesome')
-      .and.notify(callback);
+      .and.notify(next);
   });
 };
