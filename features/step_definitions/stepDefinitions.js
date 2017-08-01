7ca0414d4b6423fa150c069952397104fca54cea
Mallory <malmoo20@gmail.com>
2017-08-01 03:00:01 +0000

    updating stepdefs to cucumber2

--- features/step_definitions/stepDefinitions.js
+++ features/step_definitions/stepDefinitions.js
@@ -1,4 +1,6 @@
 // features/step_definitions/stepDefinitions.js
+var seleniumWebdriver = require('selenium-webdriver');
+var {defineSupportCode} = require('cucumber');
 var angularPage = require('../pages/homePage.js');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
@@ -6,31 +8,25 @@ var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = chai.expect;
 
-
-module.exports = function() {
-  this.Given(/^I go to "([^"]*)"$/, function(site, next) {
-    //browser.get(site);
+defineSupportCode(function({Given, When, Then}) {
+  Given('I go to {stringInDoubleQuotes}', function (site, next) {
     angularPage.go(site);
     expect(browser.getTitle()).to.eventually.equal('AngularJS — Superheroic JavaScript MVW Framework').and.notify(next);
   });
-
-  this.When(/^I add "([^"]*)" in the task field$/, function(task, next) {
-    //element(by.model('todoList.todoText')).sendKeys(task);
+       
+  When('I add {stringInDoubleQuotes} in the task field', function (task, next) {
     angularPage.addTask(task);
     next();
   });
-
-  this.When(/^I click the add button$/, function(next) {
-    //var el = element(by.css('[value="add"]'));
-    //el.click();
+  
+  When('I click the add button', function (next) {
     angularPage.submitTask();
     next();
   });
-
-  this.Then(/^I should see my new task in the list$/, function(next) {
+       
+  Then('I should see my new task in the list', function (next) {
     var todoList = angularPage.angularHomepage.todoList;
     expect(todoList.count()).to.eventually.equal(3);
-    expect(todoList.get(2).getText()).to.eventually.equal('Be Awesome')
-      .and.notify(next);
+    expect(todoList.get(2).getText()).to.eventually.equal('Be Awesome').and.notify(next);
   });
-};
+});
